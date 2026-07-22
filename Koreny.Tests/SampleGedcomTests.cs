7003cb528ee23ea5d2f73ac118f9a4a895d2c8a6
Jiří Šlachta <jiri@slachta.eu>
2026-07-22 23:19:16 +0200

    CLEANUP: Remove unused test

--- Koreny.Tests/SampleGedcomTests.cs
+++ Koreny.Tests/SampleGedcomTests.cs
@@ -6,19 +6,6 @@ namespace Koreny.Tests;
 public class SampleGedcomTests
 {
     [Fact]
-    public void Sample_ParsesWithExpectedCounts()
-    {
-        var text = SampleGedcom.ReadText();
-        var doc = new GedcomParser().Parse(text);
-
-        Assert.Equal(15, doc.Individuals.Count);
-        Assert.Equal(6, doc.Families.Count);
-
-        // Osoba se dvěma manželstvími (František @I1@ je HUSB v F1 i F2).
-        Assert.Equal(2, doc.Families.Count(f => f.HusbandId == "I1"));
-    }
-
-    [Fact]
     public void Sample_RoundTripsClean()
     {
         var text = SampleGedcom.ReadText();
